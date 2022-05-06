8d26a4490408eaa01082d5ad226b0f47e4b4cc63
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-05-06 14:06:22 -0400

    Completed code-along

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Events.Add("Strange Loops");
+            //Events.Add("Grace Hopper");
+            //Events.Add("Code with Pride");
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+        }
+    }
+}
